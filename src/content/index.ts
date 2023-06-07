ea1e1d7a818efb3e95bdf166ba6f4c0a12f0ca33
Eugen Patru <eugen.ptr@gmail.com>
2023-06-07 23:22:38 +0900

    Created dragable button which triggers a modal

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,3 +1,85 @@
 console.info('chrome-ext template-react-ts content script')  
 
+let dragging = false;
+let mouseDownPos = { x: 0, y: 0 };
+
+let button = document.createElement('button');
+button.id = 'modal-button';
+button.textContent = 'Open Modal';
+
+let modal = document.createElement('div');
+modal.id = 'modal-content';
+modal.textContent = 'This is where we call the OpenAI api and display suggestions';
+
+let textarea = document.querySelector('textarea');
+
+if (textarea) {
+    var rect = textarea.getBoundingClientRect();
+    button.style.left = rect.right + window.scrollX + 10 + 'px';
+    button.style.top = rect.top + window.scrollY - 10 + 'px';
+    button.style.display = 'block';
+
+    console.log("Textarea positioning", textarea.offsetLeft, textarea.offsetTop)
+    console.log("Button positioning", button.style.left, button.style.top)
+
+    textarea.onmousedown = (event: MouseEvent) => {
+        button.style.display = 'block';
+        console.log("Click client position", event.clientX, event.clientY)
+        console.log("Click page position", event.pageX, event.pageY)
+    }
+}
+
+function onMouseMove(event: MouseEvent): void {
+    button.style.left = event.pageX - button.offsetWidth / 2 + 'px';
+    button.style.top = event.pageY - button.offsetHeight / 2 + 'px';
+}
+
+button.onmousedown = function(event: MouseEvent): void {
+    // button.style.position = 'absolute';
+    dragging = true;
+    mouseDownPos = { x: event.clientX, y: event.clientY };
+
+    document.addEventListener('mousemove', onMouseMove);
+};
+
+// Add modal functionality
+button.onmouseup = function(event: MouseEvent): void {
+    dragging = false;
+
+    document.removeEventListener('mousemove', onMouseMove);
+    
+    const movementX = Math.abs(mouseDownPos.x - event.clientX);
+    const movementY = Math.abs(mouseDownPos.y - event.clientY);
+
+    // Consider it a click if the mouse didn't move more than 2px in either direction
+    if (movementX <= 2 && movementY <= 2) {
+        let rect = button.getBoundingClientRect();
+        modal.style.display = 'block';
+        modal.style.left = rect.left + button.offsetWidth/2 - modal.offsetWidth + 'px';
+        modal.style.top = rect.top - modal.offsetHeight + 'px';
+        
+    }
+};
+
+
+document.addEventListener('mousedown', function(event) {
+    // Check if the modal is displayed
+    if (modal.style.display === 'block') {
+        // Check if the click occurred outside the modal
+        if (!modal.contains(event.target as Node)) {
+            // Hide the modal
+            modal.style.display = 'none';
+        }
+    }
+
+    if (button.style.display === 'block') {
+        if (textarea && !textarea.contains(event.target as Node) && !button.contains(event.target as Node)) {
+            button.style.display = 'none';
+        }
+    }
+});
+
+document.body.appendChild(button);
+document.body.appendChild(modal);
+
 export {}
--- vite.config.ts
+++ vite.config.ts
@@ -6,26 +6,33 @@ import zipPack from 'vite-plugin-zip-pack';
 import manifest from './src/manifest'
 //@ts-ignore
 import {config} from './src/read_pages_folder'
+import copy from 'rollup-plugin-copy'
+export default defineConfig(({ mode }) => {  
+  return {  
+    build: {  
+      emptyOutDir: true,  
+        outDir: 'build',  
+        rollupOptions: {  
+          input: config,
+          output: {  
+            chunkFileNames: 'assets/chunk-[hash].js',  
+          },  
+      },  
+    },  
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    build: {
-      emptyOutDir: true,
-      outDir: 'build',
-      rollupOptions: {
-        input: config,
-        output: {
-          chunkFileNames: 'assets/chunk-[hash].js',
-        },
-      },
-    },
-
-    plugins: [crx({ manifest }), react(),zipPack({
-        outDir: `package`,
-        inDir: 'build',
-        // @ts-ignore
+    plugins: [
+      crx({ manifest }), 
+      react(),
+      copy({
+        targets: [
+          { src: 'src/content/index.css', dest: 'build/src/content' }
+        ]
+      }),
+      zipPack({  
+        outDir: `package`,  
+        inDir: 'build',  
         outFileName: `${manifest.short_name ?? manifest.name.replaceAll(" ", "-")}-extension-v${manifest.version}.zip`,
-      }),],
-  }
+      }),
+    ],  
+    }  
 })
